5eae6843982ee05bf6624c69464e87bef8d08590
James Coddington <james@coddington.io>
2023-04-20 13:42:47 -0500

    Added the start of the sweep function

--- analysis.py
+++ analysis.py
@@ -4,16 +4,14 @@ from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 run pip install vaderSentiment
 """
 
-analyzer = SentimentIntensityAnalyzer()
-user_messages = ["hello", "bitch", "you're an ass", "you're pretty", "Buh bye"]
+def analyze(messages):
+    analyzer = SentimentIntensityAnalyzer()
+    negative_score = 0
 
-negative_score = 0
+    for message in messages:
+        message_score = analyzer.polarity_scores(message)
+        negative_score += message_score["neg"]
+    
+    overall_negative_score = negative_score / len(messages)
 
-for message in user_messages:
-    message_score = analyzer.polarity_scores(message)
-    print("{:-<65} {}".format(message, str(message_score)))
-    negative_score += message_score["neg"]
-
-overall_negative_score = negative_score / len(user_messages)
-
-print(f"The user's overall negative score is: {overall_negative_score}")
+    return overall_negative_score
